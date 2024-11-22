b1165f40de53c75f195c3c4cb5ea2b085b2271bf
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2024-11-22 01:06:51 -0300

    feat:deserializing_urlCode

--- src/main/java/com/jeffmonteiro/redirectUrlShortener/Main.java
+++ src/main/java/com/jeffmonteiro/redirectUrlShortener/Main.java
@@ -7,15 +7,16 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
-public class Main implements RequestHandler<Map<String, String>, Map<String, String>> {
+public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
     private final S3Client s3Client = S3Client.builder().build();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public Map<String, String> handleRequest(Map<String, String> input, Context context) {
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
         String pathParameters = (String) input.get("rawPath");
         String shortUrlCode = pathParameters.replace("/", "");
 
@@ -46,6 +47,18 @@ public class Main implements RequestHandler<Map<String, String>, Map<String, Str
 
         long currentTimeInSeconds = System.currentTimeMillis() / 1000;
 
-        return null;
+        Map<String, Object> response = new HashMap<>();
+
+        if(urlData.getExpirationTime() < currentTimeInSeconds) {
+            response.put("statusCode", 410);
+            response.put("body", " This URL has expired");
+            return response;
+        }
+        response.put("statusCode", 302);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Location", urlData.getOriginalUrl());
+        response.put("headers", headers);
+
+        return response;
     }
 }
